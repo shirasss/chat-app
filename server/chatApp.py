9f2c1e7778a990830b825450463e6ae11000f0c9
shira <shirass321@gmail.com>
2023-08-23 09:38:44 +0300

    docker file

--- server/chatApp.py
+++ server/chatApp.py
@@ -1,54 +1,8 @@
-from flask import Flask, request, jsonify
-import os
-import csv
-import hashlib
-import base64
-from datetime import datetime
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
-# Load environment variables
-ROOMS_PATH = os.getenv("ROOMS_PATH")
-USERS_CSV = "users.csv"
-
-# Helper functions
-
-def verify_password(password, stored_hash):
-    # Verify the provided password against the stored hash
-    pass
-
-@app.route('/register', methods=['POST'])
-def register():
-    # Handle user registration
-    # Validate user input and add the new user to the CSV file
-    pass
-
-@app.route('/login', methods=['POST'])
-def login():
-    # Handle user login
-    # Validate user credentials and generate a session token
-    pass
-
-@app.route('/logout', methods=['GET'])
-def logout():
-    # Handle user logout by invalidating the session token
-    pass
-
-@app.route('/lobby', methods=['GET'])
-def lobby():
-    # Display the main lobby page where users can create or enter chat rooms
-    pass
-
-@app.route('/chat/<room>', methods=['GET'])
-def chat_room(room):
-    # Display the specified chat room with all messages sent
-    pass
-
-@app.route('/api/chat/<room>', methods=['GET'])
-def api_chat_updates(room):
-    # API endpoint for updating chat content in real-time
-    pass
-
-# Main function to run the application
+ 
+@app.route('/')
+def home():
+   return render_template('home.html')
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run()
