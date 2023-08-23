26076a66312cf63e1a3963d593710eaa108f03ea
rach6216 <rach0556776216@gmail.com>
2023-08-23 12:20:02 +0300

    delete img

--- chatApp.py
+++ chatApp.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
  
 @app.route('/')
-def loginPage():
-    return render_template('login.html')
+def homePage():
+    return render_template('register.html')
 if __name__ == '__main__':
    app.run(host="0.0.0.0")
--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 docker build -t  chat-img .
-docker run -d -p 8000:5000 chat-img
+docker run -d --name chat-run -p 8000:5000 chat-img 
