5410b07daf1d430d4e5bd5f25b2f461cc48aab1c
rach6216 <rach0556776216@gmail.com>
2023-08-23 11:52:42 +0300

    build docker

--- chatApp.py
+++ chatApp.py
@@ -14,6 +14,6 @@ app = Flask(__name__)
  
 @app.route('/')
 def home():
-    return render_template('lobby.html')
+    return "render_template('./templates/register.html')"
 if __name__ == '__main__':
    app.run(host="0.0.0.0")
---
+++ init.sh
@@ -0,0 +1,2 @@
+docker build -t  chat-img .
+docker run -d -p 8000:5000 chat-img
