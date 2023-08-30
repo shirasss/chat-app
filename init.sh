9142be155cf03956e31365803d77fe9a9dd15f43
rach6216 <rach0556776216@gmail.com>
2023-08-30 14:31:03 +0300

    syntax

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker volume create chat-app-data
-docker build -t  chat-app:v0.0.0 .
+docker build -t  chat-app: .
 # docker run --name chat-run -p 5000:5000 chat-app 
-docker run -v chat-app-data:/chatApp/data -p 5000:5000 --name chat-run chat-app:v0.0.0
+docker run -v chat-app-data:/chatApp/data -p 5000:5000 --name chat-run chat-app:
