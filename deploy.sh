85ec3a6cca1a8fe7edbe3413d5da74d1e138606f
rach6216 <rach0556776216@gmail.com>
2023-08-30 10:14:06 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the version and commit hash from the user
+echo "Enter the version number: "
+read version
+echo "Enter the commit hash: "
+read commit_hash
+
+app-name='chat-app'
+# Build the Docker image
+docker build -t ${app-name}:${version} .
+
+# Tag the image with the commit hash
+git tag v${version} ${commit_hash}
+
+# Check if the image was pushed successfully
+if [ $? -ne 0 ]; then
+  echo "Error pushing image to GitHub"
+  exit 1
+fi
+
+# Success!
+echo "Image pushed to GitHub successfully"
